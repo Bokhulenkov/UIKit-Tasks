3d9561593980dc51bde4815888ecd714583e9322
Bokhulenkov <bokhulenkov@gmail.com>
2024-09-26 10:28:08 +0300

    Initial Commit

---
+++ UIKit Tasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKit Tasks
+//
+//  Created by Alexander Bokhulenkov on 26.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
